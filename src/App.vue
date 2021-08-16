2b23c66fe2bb7aede2cc7c7ddffe415cee4acb7d
bakolaszlo <bakolaszlo999@gmail.com>
2021-08-16 18:14:45 +0300

    Header done?

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="d-flex justify-content-end align-items-center">
+      <a class="head-ref" href="">Gmail</a>
+      <a class="head-ref" href="">Images</a>
+      <a class="dots">
+        <svg class="headersvg" focusable="false" viewBox="0 0 24 24">
+          <path
+            d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"
+          ></path>
+        </svg>
+      </a>
+      <b-button variant="primary">Sign in</b-button>
     </div>
-    <router-view />
   </div>
 </template>
 
@@ -15,6 +22,27 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.d-flex {
+  padding: 6px;
+}
+
+.head-ref {
+  text-align: center;
+  padding-right: 1em;
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.dots {
+  height: 24px;
+  fill: #5f6368;
+  display: inherit;
+  padding-top: 0.15em;
+  text-align: center;
+  padding-right: 1em;
+  text-decoration: none;
 }
 
 #nav {
